14c6eea1b57d1ea9f7685fd60b4c07b47f27bdb3
wuchien <fe19ng@gmail.com>
2018-06-27 16:09:55 +0800

    tt1

--- test/test.js
+++ test/test.js
@@ -4,5 +4,10 @@ describe("Array", function() {
     it("should return -1 when the value is not present", function() {
       assert.equal(-1, [1, 2, 3].indexOf(4));
     });
+	});
+	describe("#indexOf()22", function() {
+    it("should return -1 when the value is not present", function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
   });
 });
